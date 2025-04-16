1d4d6332b892c3d1e542c13f1e9e0f209f09bbc7
koto-thing <gotoukenta62@gmail.com>
2025-04-17 02:13:17 +0900

    Unityのビルドを追加

---
+++ my-website/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import UnityViewer from "./components/UnityViewer";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>ポートフォリオサイト</h1>
+      <UnityViewer />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-website/src/components/UnityViewer.js
@@ -0,0 +1,18 @@
+﻿import React from "react";  
+
+const UnityViewer = () => {
+    return (
+        <div style = {{ width: "100%", height: "600px"}}>
+            <iframe
+                title = "Unity WebGL"
+                src = "/unity/index.html"
+                width = "100%"
+                height = "100%"
+                frameBorder = "0"
+                allowFullScreen
+                ></iframe>
+        </div>
+    );
+};
+
+export default UnityViewer;
